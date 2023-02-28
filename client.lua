88d55b22987a8994b7259fb8ac6bed12da060b28
Erko <51840953+Erkoq@users.noreply.github.com>
2023-02-28 18:39:06 +0000

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,32 +1,41 @@
-local chance, skillGap, factor, taskInProcess, calm, activeTasks = 0, 20, 1.0, false, true, 0
 
-function openGui(sentLength, taskID, namesent, chancesent, skillGapSent)
-    SetNuiFocus(true, false)
-    SendNUIMessage({ runProgress = true, Length = sentLength, Task = taskID, name = namesent, chance = chancesent, skillGap = skillGapSent })
+
+local chance = 0
+local skillGap = 20
+local factor = 1.0
+local taskInProcess = false
+local calm = true
+
+function openGui(sentLength,taskID,namesent,chancesent,skillGapSent)
+    guiEnabled = true
+    SetNuiFocus(guiEnabled,false)
+  --  print("taskbar started")
+    SendNUIMessage({runProgress = true, Length = sentLength, Task = taskID, name = namesent, chance = chancesent, skillGap = skillGapSent})
 end
-
-function updateGui(sentLength, taskID, namesent, chancesent, skillGapSent)
-    SendNUIMessage({ runUpdate = true, Length = sentLength, Task = taskID, name = namesent, chance = chancesent, skillGap = skillGapSent })
+function updateGui(sentLength,taskID,namesent,chancesent,skillGapSent)
+    SendNUIMessage({runUpdate = true, Length = sentLength, Task = taskID, name = namesent, chance = chancesent, skillGap = skillGapSent})
 end
-
+local activeTasks = 0
 function closeGuiFail()
-    SetNuiFocus(false, false)
-    SendNUIMessage({ closeFail = true })
+    guiEnabled = false
+    SetNuiFocus(guiEnabled,false)
+    SendNUIMessage({closeFail = true})
 end
-
 function closeGui()
-    SetNuiFocus(false, false)
-    SendNUIMessage({ closeProgress = true })
+    guiEnabled = false
+    SetNuiFocus(guiEnabled,false)
+    SendNUIMessage({closeProgress = true})
 end
 
 function closeNormalGui()
-    SetNuiFocus(false, false)
+    guiEnabled = false
+    SetNuiFocus(guiEnabled, false)
 end
-
+  
 RegisterNUICallback('taskCancel', function(data, cb)
-    closeGui()
-    activeTasks = 2
-    FactorFunction(false)
+  closeGui()
+  activeTasks = 2
+  FactorFunction(false)
 end)
 
 RegisterNUICallback('taskEnd', function(data, cb)
@@ -34,6 +43,7 @@ RegisterNUICallback('taskEnd', function(data, cb)
     activeTasks = 3
     factor = 1.0
 end)
+
 
 function FactorFunction(pos)
     if not pos then
@@ -43,52 +53,61 @@ function FactorFunction(pos)
         end
         if factor == 0.5 and calm then
             calm = false
+            TriggerEvent("notification","You are frustrated", 2)
         end
-        Wait(15000)
-        FactorFunction(true)
+        TriggerEvent("factor:restore")        
     else
         if factor > 1.0 or factor == 0.9 then
             if not calm then
+                TriggerEvent("notification","You are calm again")
                 calm = true
-            end
+            end            
             factor = 1.0
             return
         end
         factor = factor + 0.1
-    end
+    end    
 end
 
-function taskBar(difficulty, skillGapSent)
-    local name = 'E'
-    local taskIdentifier = 'taskid' .. math.random(1000000)
-    local length = math.ceil(difficulty * factor)
-    skillGap = skillGapSent
+RegisterNetEvent('factor:restore')
+AddEventHandler('factor:restore', function()
+    Wait(15000)
+    FactorFunction(true)
+end)
 
+
+function taskBar(difficulty,skillGapSent)
+    Wait(100)
+    skillGap = skillGapSent
     if skillGap < 5 then
         skillGap = 5
     end
-
+    local name = "E"
+    local playerPed = PlayerPedId()
     if taskInProcess then
         return 100
     end
-
     FactorFunction(false)
-    chance = math.random(15, 90)
+    chance = math.random(15,90)
+
+    local length = math.ceil(difficulty * factor)
+
     taskInProcess = true
-    openGui(length, taskIdentifier, name, chance, skillGap)
+    local taskIdentifier = "taskid" .. math.random(1000000)
+    openGui(length,taskIdentifier,name,chance,skillGap)
     activeTasks = 1
 
     while activeTasks == 1 do
-        Wait(1)
+        Citizen.Wait(1)
     end
 
     if activeTasks == 2 then
         closeGui()
         taskInProcess = false
         return 0
-    end
-
-    closeGui()
-    taskInProcess = false
-    return 100
+    else
+        closeGui()
+        taskInProcess = false
+        return 100
+    end 
 end
